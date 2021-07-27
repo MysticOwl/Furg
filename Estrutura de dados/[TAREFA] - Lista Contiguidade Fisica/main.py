f175b331b3f31f67ad2a90abe0d587fbb3d48066
Thiago Copello <43577388+MysticOwl@users.noreply.github.com>
2021-07-27 01:02:22 -0300

    Ano 2 | Furg-2021

---
+++ Estrutura de dados/[TAREFA] - Lista Contiguidade Fisica/main.py
@@ -0,0 +1,18 @@
+from _class import *
+from product import *
+
+lista = Lista(10)
+
+lista.startLista(5,0)
+print(lista.vetor)
+lista.insert(6,11)
+print(lista.vetor)
+lista.insert(2,256)
+print(lista.vetor)
+lista.insert(1,32)
+print(lista.vetor)
+lista.insert(9,4)
+print(lista.vetor)
+lista.insert(1,5)
+print(lista.vetor)
+print(lista)
