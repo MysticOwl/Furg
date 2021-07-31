7126add6a4c1e923438bd91f52b629c90ff7c2dc
Thiago Copello <43577388+MysticOwl@users.noreply.github.com>
2021-07-30 23:31:32 -0300

    Calculadora V-1.2 | Falta medição de tempo.

--- Algoritimo/[TAREFA] - Calculadora duas operações/Calculadora_duas_operacoes.py
+++ Algoritimo/[TAREFA] - Calculadora duas operações/Calculadora_duas_operacoes.py
@@ -1,20 +1,42 @@
-def mult(x,y):
-    if (x < 10 ):
-        return (x * y)
-    n = (len(str(x))-1)
-    soma = ((x//10**n) * y)*10**n
-    x = x%10**n
+def mult(x, y):
+    
+    if x < 10 and y < 10:
+        return x * y
+        
+    num1_len = len(str(x))
+    num2_len = len(str(y))
 
-    return soma + mult(x,y)
+    n = max(num1_len,num2_len)
+    
+    nby2 = round(n/2)
+
+    num1 = x // (10 ** nby2)
+    rem1 = x % (10 ** nby2)
+
+    num2 = y // (10 ** nby2)
+    rem2 = y % (10 ** nby2)
+
+    ac = mult(num1, num2)
+    bd = mult(rem1, rem2)
+    ad_plus_bc = mult(num1 + rem1, num2 + rem2) - ac - bd
+
+    return (10 ** (2*nby2))*ac + (10 ** nby2)*ad_plus_bc + bd
 
 def soma(x,y):
-    if x > y:
-        while y > 0:
-            x += 1
-            y -= 1
-        return x
-    else:
-        while x > 0:
-            y += 1
-            x -= 1
-        return y
+    
+    if (len(str(x))) == 1 and (len(str(y))) == 1:
+        return x*y
+
+    n = max(len(str(x)),len(str(y)))
+    n2 = round(n/2)
+    
+    x0 = x // 10**n2
+    x1 = x % 10**n2
+
+    y0 = y // 10**n2
+    y1 = y % 10**n2
+
+    x = soma(x0,x1)
+    y = soma(y0,y1)
+
+    return (x0 * 10**n2 + x1) + (y0 * 10**n2 + y1)
