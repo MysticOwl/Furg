9943cc856b4a1592e2f989b276cf350089e896c6
Thiago Copello <43577388+MysticOwl@users.noreply.github.com>
2021-08-26 17:01:52 -0300

    Busca binaria implementada

--- Estrutura de dados/[TAREFA] - Tabela/Main.py
+++ Estrutura de dados/[TAREFA] - Tabela/Main.py
@@ -2,7 +2,7 @@ from Class import Carro
 from Structs import Tabela
 
 
-tabela = Tabela(1)
+tabela = Tabela(4)
 carro = Carro()
 carro2 = Carro()
 carro3 = Carro()
@@ -10,22 +10,20 @@ carro3 = Carro()
 carro.setAno(1994)
 carro.setDono('Anderson')
 carro.setModelo('Golf')
-carro.setPlaca('ABC123')
+carro.setPlaca('ABC124')
 
 carro2.setAno(1995)
 carro2.setDono('Ricado')
 carro2.setModelo('Golf')
-carro2.setPlaca('ABC124')
+carro2.setPlaca('DFG124')
 
 carro3.setAno(1996)
 carro3.setDono('Fernando')
 carro3.setModelo('Ford K')
-carro3.setPlaca('DFG124')
+carro3.setPlaca('ABC123')
 
 tabela.insert(carro)
 tabela.insert(carro2)
 tabela.insert(carro3)
 
-tabela._search(carro3)
-
-print(tabela)
+print(tabela._binarySearch('ABC123'))
--- Estrutura de dados/[TAREFA] - Tabela/Structs.py
+++ Estrutura de dados/[TAREFA] - Tabela/Structs.py
@@ -8,6 +8,8 @@ class Tabela:
     
     def __repr__(self):
         string = ''
+        if self._size == 0:
+            return 'Tabela Vazia'
         for i in range(self._ini, self._size + 1):
             string += str(self._value[i]) + '\n'
         return string
@@ -24,34 +26,63 @@ class Tabela:
         else:
             return False
 
-    def _search(self,value):
+    def _linearSearch(self,value):
         if (not self.isEmpty()):
             for i in range(self._ini, self._size + 1):
-                if self._key[i] == value.getPlaca():
+                if self._key[i] == value:
                     return i
         return 0
+    
+    def _binarySearch(self,value):
+        if (not self.isEmpty()):
+            self._sort
+            aux_ini = self._ini
+            aux_fim = self._size
+            while aux_ini <= aux_fim:
+                middle = (aux_ini + aux_fim)//2
+                if str(self._key[middle]) == value:
+                    return self._value[middle]
+                else:
+                    if str(self._key[middle]) < value:
+                        aux_fim = middle - 1
+                    else:
+                        aux_ini = middle + 1
+            return 0
 
-    def look(self,value):
-        index = self._search(value)
-        if index > 0:
-            return self._value[index]
+    def look(self,value, param):
+        if param == 'l' or param == 'L':
+            index = self._linearSearch(value)
+            if index > 0:
+                return self._value[index]
+        elif param == 'b' or param == 'B':
+            self._binarySearch(value)
+        else:
+            return False
     
     def _sort(self):
-        pass
+        aux_key = ''
+        aux_value = ''
+        for i in range(self._ini, self._size + 1):
+            for j in range(self._ini , self._size + 1):
+                if self._key[i] < self._key[j]:
+                    aux_key = self._key[i]
+                    self._key[i] = self._key[j]
+                    self._key[j] = aux_key
+                    aux_value = self._value[i]
+                    self._value[i] = self._value[j]
+                    self._value[j] = aux_value                    
 
     def insert(self,value):
-        index = self._search(value)
+        index = self._linearSearch(value)
         if index > 0:
             self._value[index] = value
         elif (not self.isFull()):
             self._size += 1
             self._key[self._size] = value.getPlaca()
             self._value[self._size] = value
-        
-        self._sort
     
     def remove(self,value):
-        index = self._search(value)
+        index = self._linearSearch(value)
         if index > 0:
             for i in range(index, self._size + 1):           
                 self._key[i] = self._key[i+1]
@@ -59,3 +90,6 @@ class Tabela:
             self._size -= 1
         else:
             return False
+    
+    def destroy(self):
+        self._size = 0
