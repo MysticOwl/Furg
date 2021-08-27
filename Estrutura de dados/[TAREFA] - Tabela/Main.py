d815a7578caa944d67a7da4bdc8359747ffcc256
Thiago Copello <43577388+MysticOwl@users.noreply.github.com>
2021-08-26 21:57:42 -0300

    Main V - 1.1

--- Estrutura de dados/[TAREFA] - Tabela/Main.py
+++ Estrutura de dados/[TAREFA] - Tabela/Main.py
@@ -1,6 +1,6 @@
 from Class import Carro
 from Structs import Tabela
-import os , platform
+import os , platform, time
 
 so = platform.system()
 
@@ -16,6 +16,7 @@ while True:
     print('2 - Remover um item.')
     print('3 - Mostrar um item.')
     print('4 - Destuir a tabela.')
+    print('5 - Encerrar programa.')
     print(bar)
     print('Tabela atual:\n')
     print(tabela)
@@ -37,31 +38,44 @@ while True:
             print('Tabela cheia.')
             print(line)
     if oper == '2':
-        print(line)
-        if (tabela.remove(input('Digite a placa do carro que deseja remover: '))):
+        if (tabela.isEmpty()):
+            print('ERROR: Tabela vazia.')
+        elif (tabela.remove(input('Digite a placa do carro que deseja remover: '))):
             print('Carro removido.')
         else:
             print('Placa não encontrada.')
     if oper == '3':
         print(line)
-        placa = input('Digite a placa do carro: ')
-        while True:
-            metodo = input('Método de perquisa [B]inario [S]equencial: ')
-            if (metodo == 'b' or metodo == 'B') or (metodo == 's' or metodo == 'S'):
-                print(tabela.look(placa,metodo))
-                break
-            else:
-                print('Comando invalido')
+        if (tabela.isEmpty()):
+            print('ERROR: Tabela vazia.')
+        else:
+            placa = input('Digite a placa do carro: ')
+            while True:
+                metodo = input('Método de perquisa [B]inario [S]equencial: ')
+                print(line)
+                if metodo in ['b','B','s','S']:
+                    if (tabela.look(placa,metodo)):
+                        print(tabela.look(placa,metodo))
+                    else:
+                        print('Placa não encontrada')
+                    break
+                else:
+                    print('Comando invalido')
     if oper == '4':
         print(line)
-        destroy = str(input('Destruir a tabela: [S]im [N]ão: '))
-        if destroy == 's' or destroy == 'S':
-            tabela.destroy()
-            print('Tabela destruida.')
+        if (tabela.isEmpty()):
+            print('ERROR: Tabela vazia.')
+        else:
+            destroy = str(input('Destruir a tabela: [S]im [N]ão: '))
+            if destroy == 's' or destroy == 'S':
+                tabela.destroy()
+                print('Tabela destruida.')
     print(line)
-    out = input('Deseja encerrar o programa [S]im [N]ão:')
-    if out == 's' or out == 'S':
-        break
+    if oper == '5':
+        out = input('Deseja encerrar o programa [S]im [N]ão:')
+        if out == 's' or out == 'S':
+            break
+    time.sleep(1.3)
     if so == 'Windows':
         os.system('cls')
     else:
